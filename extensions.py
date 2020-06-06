b833bea7c69ed4c6762b605b38a8d089cbb723a7
Георгий Рыбкин <rybkin.georgy@yandex.ru>
2020-06-07 01:14:43 +0300

    release

--- extensions.py
+++ extensions.py
@@ -7,6 +7,7 @@ from io import BytesIO
 
 class GoogleNewsURLDumper:
     def _get_data(self, request_text):
+        print(request_text)
         soup = BeautifulSoup(request_text, 'lxml')
         data = soup.find_all('div', class_='ts')
         if not data:
