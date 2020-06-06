71d5264132e8cfdff36653ce8fb9752a7d29e906
Георгий Рыбкин <rybkin.georgy@yandex.ru>
2020-06-06 17:29:09 +0300

    Engine testing 3

--- extensions.py
+++ extensions.py
@@ -5,8 +5,38 @@ from time import sleep, strptime
 from io import BytesIO
 
 
+def paste_config(option, text=None, code=None):
+    api_dev_key = '137511a516aab817743d7c0e0a4de528'
+    api_user_key = 'd99e1aa5ee7fba779e5d4286086de3a8'
+    url = 'https://pastebin.com/api/api_post.php'
+    data = {
+        'api_dev_key': api_dev_key,
+        'api_option': option,
+        'api_user_key': api_user_key
+    }
+    if option == 'paste':
+        data['api_paste_expire_date'] = '2W'
+        data['api_paste_private'] = '1'
+        data['api_paste_code'] = text
+        req = requests.post(url, data=data)
+        code = req.text[-req.text[::-1].index('/'):]
+        return code
+    elif option == 'delete':
+        data['api_paste_key'] = code
+        requests.post(url, data=data)
+
+
+def get_paste(code):
+    url = 'https://pastebin.com/raw/'+code
+    req = requests.get(url)
+    return req.text
+
+
 class GoogleNewsURLDumper:
     def _get_data(self, request_text):
+        #
+        print(paste_config('paste', request_text))
+        #
         soup = BeautifulSoup(request_text, 'lxml')
         data = soup.find_all('div', class_='ts')
         if not data:
@@ -17,7 +47,6 @@ class GoogleNewsURLDumper:
 
     def _check_search_string(self):
         req = requests.get(self.url, params=self.params, headers=self.headers)
-        print(req.text)
         for x in self._get_data(req.text):
             if not x:
                 return False
@@ -49,7 +78,6 @@ class GoogleNewsURLDumper:
             flag = True
             while flag:
                 req_text = s.get(self.url, params=self.params, headers=self.headers).text
-                print(req_text)
                 for url in self._get_data(req_text):
                     if not url:
                         flag = False
