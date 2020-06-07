8e95974f69f2020e00f70eed98e822fab07f05d4
Георгий Рыбкин <rybkin.georgy@yandex.ru>
2020-06-08 01:36:56 +0300

    add heroku port

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from app import create_app
 from config import Config
+from os import getenv
 
 app = create_app(Config)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    # app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=getenv('PORT'))
